234e6ec921026038de8f578d462ba265e5b8d5d7
Coline <coline.cantelou@gmail.com>
2023-03-24 15:10:11 +0100

    modify contact modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,3 +1,5 @@
+const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
@@ -12,7 +14,7 @@ function displayForm(name) {
     const querySelector = document.querySelector(".modal");
     const header = querySelector.querySelector("h2");
     const button = document.createElement('button');
-    const inputType = {"prénom": 'input', "Nom": 'input', 'Email': 'input', 'Votre message': 'textarea'}
+    const inputType = {"Prénom": 'input', "Nom": 'input', 'Email': 'input', 'Votre message': 'textarea'}
 
     setAttributes(button,{"class":"contact_button", "onclick": 'valide()'})
     button.textContent = "Envoyer";
@@ -22,13 +24,15 @@ function displayForm(name) {
 
 }
 
+// Create form
 function createForm(obj) {
     const form = document.createElement( 'form' );
     for (const property in obj) {
         const div = document.createElement( 'div' );
         const label = document.createElement('label');
         const input = document.createElement(obj[property]);
-        setAttributes(input,{"name":property})
+        setAttributes(div,{"class":"formData","data-error":"error","data-error-visible":"false"})
+        setAttributes(input,{"name":property, "class":"text-control"})
         label.textContent = property;
         div.append(label, input)
         form.append(div);
@@ -36,12 +40,50 @@ function createForm(obj) {
     return form
 }
 
+//if click on valid button 
 function valide() {
     const input = document.querySelectorAll('input');
     const textarea = document.querySelector('textarea');
-    for (let i = 0; i < input.length; i++) {
-        console.log(input[i].name , ": ", input[i].value);
+    const formData = document.querySelectorAll(".formData");
+    var valide = true;
+    for (let i = 0; i < formData.length; i++) {
+        console.log(formData[i]);
+      formData[i].dataset.errorVisible = false;
+      valide = true;
     }
     
+    for (let i = 0; i < input.length; i++) {
+        const value = input[i].value;
+        switch (input[i].name) {
+            case 'Prénom':
+            case 'Nom':
+                if (value == "") {
+                    errorMessage('Champ obligatoire', formData[i])
+                }
+                break;
+            case 'Email':
+                if (!regexEmail.test(value)){
+                    errorMessage('email non valide', formData[i])
+                }
+                break;
+        }
+    }
+    
+    if (textarea.value  == "") {
+        errorMessage('Champ obligatoire', textarea.parentElement)
+    }
+    else if (valide == true) {
+        for (let i = 0; i < input.length; i++) {
+    console.log(input[i].name , ": ", input[i].value);
+        }
     console.log(textarea.name , ": ", textarea.value);
+    }
+    
 }
+
+// function error message
+function errorMessage(message, formData){
+    formData.dataset.error = message;
+    formData.dataset.errorVisible = true;
+    valide = false;
+  }
